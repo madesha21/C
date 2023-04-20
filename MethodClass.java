5271e3fc6468811d5c698822006663181265b15d
madesha21 <madesharamegowda11@gmail.com>
2023-04-20 21:45:46 +0530

    This is Simple object creation

---
+++ MethodClass.java
@@ -0,0 +1,18 @@
+class A
+{
+	public void DisplayMsg()
+	{
+		System.out.println("Hello java Developer..");
+	}
+}
+public class MethodClass
+{
+	public static void main(String args[])
+	{
+		A ob=new A();
+		ob.DisplayMsg();
+
+	}
+}
+
+
